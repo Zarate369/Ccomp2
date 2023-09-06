d346d61c429312865fd6acfc3bbf283790781007
Zarate369 <143094166+Zarate369@users.noreply.github.com>
2023-09-05 23:49:52 -0500

    Create Ejercicio2_21.cpp

---
+++ semana_3/Ejercicio2_21.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "  CCC   +      +" << endl;
+    cout << " C      +      +" << endl;
+    cout << "C     +++++  +++++" << endl;
+    cout << " C      +      +" << endl;
+    cout << "  CCC   +      +" << endl;
+}
