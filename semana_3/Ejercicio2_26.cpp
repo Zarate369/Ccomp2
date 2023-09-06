d42cea53d39c65f450cd25b3172bb5a653c70aad
Zarate369 <143094166+Zarate369@users.noreply.github.com>
2023-09-06 00:52:32 -0500

    Create Ejercicio2_26.cpp

---
+++ semana_3/Ejercicio2_26.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "* * * * * * * *" << endl;
+    cout << " * * * * * * * *" << endl;
+    cout << "* * * * * * * *" << endl;
+    cout << " * * * * * * * *" << endl;
+    cout << "* * * * * * * *" << endl;
+    cout << " * * * * * * * *" << endl;
+    cout << "* * * * * * * *" << endl;
+    cout << " * * * * * * * *\n" << endl;
+
+    for (int i = 1; i <= 4 ; i++){
+        cout << "* * * * * * * *\n * * * * * * * *" << endl;
+    }
+}
