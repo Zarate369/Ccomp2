f1a2493601b5956252d6d4a5f97e57f8a9eaed69
Zarate369 <143094166+Zarate369@users.noreply.github.com>
2023-09-05 23:50:19 -0500

    Create Ejercicio2_25.cpp

---
+++ semana_3/Ejercicio2_25.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1{0};
+    int num2{0};
+    int num3{0};
+    int fact{0};
+
+
+    cout <<"Ingresa el primer numero: ";
+    cin >> num1;
+
+    cout <<"Ingresa el segundo numero: ";
+    cin >> num2;
+
+    cout <<"Ingresa el tercer numero: ";
+    cin >> num3;
+
+    if ((num1%num3)==0){
+        cout << "El numero " << num1 << " es multiplo de " << num3 << endl;
+    }
+    else {
+        cout << "El numero " << num1 << " no es multiplo de " << num3 << endl;
+    }
+
+    if ((num2%num3)==0){
+        cout << "El numero " << num2 << " es multiplo de " << num3 << endl;
+    }
+    else {
+        cout << "El numero " << num2 << " no es multiplo de " << num3 << endl;
+    }
+    
+
+}
