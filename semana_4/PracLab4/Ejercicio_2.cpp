6aa624146a797d7e612c4c6cb02536a072b996a6
Zarate369 <143094166+Zarate369@users.noreply.github.com>
2023-09-16 18:22:10 -0500

    Create Ejercicio_2.cpp

---
+++ semana_4/PracLab4/Ejercicio_2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std; 
+//Los teminos en la secuencia fibonacci no debe superar 4 000 000, encontrar la suma de pares
+int main()
+{
+	
+	int i{0};
+	int cont{0};
+	int sum{1};
+	int num=4000000;
+    cout<<"Numeros Fibonacci:";
+	do{
+		int temp{0};
+		temp=i;
+		i=sum+i;
+		if(i<num){
+			cout<<"\n"<<i;
+		}
+		
+		sum=temp;
+		if(i%2==0 && i<num){
+			cont=cont+i;
+		}
+	}while(i<=num);
+	cout << "\n\nSuma de pares: "<< cont;
+}
